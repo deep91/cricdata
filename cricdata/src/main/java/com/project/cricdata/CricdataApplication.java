9f65c382b45005af58069fb3c16462c023761d21
PradeepSivakumar <Pradeep.Kannan@powerschool.com>
2019-10-14 16:41:06 +0530

    SpringBoot Project Addedd

---
+++ cricdata/src/main/java/com/project/cricdata/CricdataApplication.java
@@ -0,0 +1,13 @@
+package com.project.cricdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricdataApplication.class, args);
+	}
+
+}
---
+++ cricdata/src/test/java/com/project/cricdata/CricdataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.cricdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CricdataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
